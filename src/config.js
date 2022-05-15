41f9c7fe88ea0c9f7ac995b6bdb6f7238be9f480
Tanishq Sangwan <tanishqsangwan7@gmail.com>
2022-05-15 13:01:01 +0530

    [feature] Add a support command

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 const config = {
   HUB_ID: 972081627129851915,
-  FEATURE_REQUESTS: 974961267729121340
+  FEATURE_REQUESTS: 974961267729121340,
+  DISCORD_INVITE: "https://discord.gg/he8R8yvBpM",
+  GITHUB_URL: "https://github.com/Sadashii/GameBot"
 }
 
-module.exports = config
+module.exports = {config}
---
+++ src/functions/information/support.js
@@ -0,0 +1,47 @@
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const {MessageActionRow, MessageButton, MessageEmbed} = require("discord.js");
+const {COLORS} = require("../../utils/data");
+const {config} = require("../../config")
+
+const command = new SlashCommandBuilder()
+  .setName("support")
+  .setDescription("Support the bot by various means.")
+
+
+module.exports = {
+  type: "COMMAND",
+  data: command,
+  enabled: true,
+  async execute (client, interaction, logger) {
+    const embed = new MessageEmbed()
+      .setColor(COLORS.INFO)
+      .setTitle("Support GameBot")
+      .setThumbnail(client.user.avatarURL())
+      .setDescription("Although GameBot is free, it is still a work in progress which requires time and money to keep it online. You can support the development of GameBot by various means.")
+    
+    const component = new MessageActionRow()
+      .addComponents(
+        new MessageButton()
+          .setURL("https://buymeacoffee.com/Sadashii")
+          .setLabel("Buy me a coffee!")
+          .setStyle("LINK")
+      )
+      .addComponents(
+        new MessageButton()
+          .setURL(config.DISCORD_INVITE)
+          .setLabel("Join the Discord server!")
+          .setStyle("LINK")
+      )
+      .addComponents(
+        new MessageButton()
+          .setURL(config.GITHUB_URL)
+          .setLabel("Contribute to code!")
+          .setStyle("LINK")
+      )
+    
+    await interaction.reply({
+      embeds: [embed],
+      components: [component]
+    });
+  },
+};
